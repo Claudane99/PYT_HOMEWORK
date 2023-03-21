4177189a8be3284e74b2c0498df46a6883c09ecb
Chang XianCheng <xc_chang0412@hotmail.com>
2023-03-21 21:13:01 +0800

    Variable Homework

---
+++ solution_variable.py
@@ -0,0 +1,64 @@
+
+Code_letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+#1
+beeps_1 = 2
+boops_1 = 6
+total_1 = beeps_1 + boops_1
+Code_letter_1 = Code_letter[total_1-1]
+#2
+beeps_2 = 0
+boops_2 = 5
+total_2 = beeps_2 + boops_2
+Code_letter_2 = Code_letter[total_2-1]
+#3
+beeps_3 = 9
+boops_3 = 3
+total_3 = beeps_3 + boops_3
+Code_letter_3 = Code_letter[total_3-1]
+#4
+beeps_4 = 4
+boops_4 = 8
+total_4 = beeps_4 + boops_4
+Code_letter_4 = Code_letter[total_4-1]
+#5
+beeps_5 = 10
+boops_5 = 5
+total_5 = beeps_5 + boops_5
+Code_letter_5 = Code_letter[total_5-1]
+#6
+beeps_6 = 11
+boops_6 = 12
+total_6 = beeps_6 + boops_6
+Code_letter_6 = Code_letter[total_6-1]
+#7
+beeps_7 = 5
+boops_7 = 5
+total_7 = beeps_7 + boops_7
+Code_letter_7 = Code_letter[total_7-1]
+#8
+beeps_8 = 1
+boops_8 = 17
+total_8 = beeps_8 + boops_8
+Code_letter_8 = Code_letter[total_8-1]
+#9
+beeps_9 = 5
+boops_9 = 7
+total_9 = beeps_9 + boops_9
+Code_letter_9 = Code_letter[total_9-1]
+#10
+beeps_10 = 4
+boops_10 = 0
+total_10 = beeps_10 + boops_10
+Code_letter_10 = Code_letter[total_10-1]
+
+#Outcome
+print(Code_letter_1)
+print(Code_letter_2)
+print(Code_letter_3)
+print(Code_letter_4)
+print(Code_letter_5)
+print(Code_letter_6)
+print(Code_letter_7)
+print(Code_letter_8)
+print(Code_letter_9)
+print(Code_letter_10)
